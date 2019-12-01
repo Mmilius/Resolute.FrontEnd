09f47ff0e9ee7f74384bc4a55c66fe2cfc834849
github username <github email address>
2019-11-30 19:01:03 -0700

    add delete alert, add edit form to resolutions, fix main image

--- src/components/ResolutionCard.vue
+++ src/components/ResolutionCard.vue
@@ -1,6 +1,15 @@
 <template>
     <div class="resolution-card">
     <section class="resolution-section">
+         <div v-if="show" class="edit-resolution">
+      <form @submit="editResolution" class="modified-resolution">
+        <input class="form-input" type="text" name="image" :value="image"/>
+        <input class="form-input" type="text" name="realm_id" :value="realm"/>
+        <input class="form-input" type="text" name="goal" :value="goal" />
+        <textarea class="form-input" name="motivation" :value="motivation"></textarea>
+        <input type="submit" class="edit-resolution-button" value="Save Edits"/>
+      </form>
+      </div>
         <img class="resolution-image" v-bind:src="resolution.image"/>
          <h3>{{resolution.realm.realm}}</h3>
         <h3>{{resolution.goal}}</h3> 
@@ -8,8 +17,9 @@
         <button @click="$emit('report-resolution', id)" class="report-button">Make a Report</button>
         <br>
         <br>
-        <font-awesome-icon icon="edit"  @click="$emit('edit-resolution', resolution.id)" class="edit-button" />
+        <font-awesome-icon icon="edit"  @click="toggleEditForm" class="edit-button" />
         <font-awesome-icon icon="trash" @click="deleteResolution(resolution.id)" class="delete-button" />
+        
 </section>
     </div>
 </template>
@@ -22,26 +32,35 @@ export default {
             required: true,
         }
     },
-    index: {
-        type: Number,
-    },
 
     data(){
         return {
+            show: false,
             "id": this.resolution.id,
             "goal": this.resolution.goal,
             "motivation": this.resolution.motivation,
+            "image": this.resolution.image,
+            "realm": this.resolution.realm.realm,
         }
     },
     methods: {
         deleteResolution(id){
+            if(confirm('Delete this resolution?'))
             this.$emit('deleteResolution', id)
-        }
+        },
+         toggleEditForm(){
+          this.show !== true
+          ? this.show = true
+          : this.show = false
+      },
+        editResolution(resolution){
+            this.$emit("editResolution", resolution)
+        },
     }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Noto+Sans+SC&display=swap');
 .resolution-image{
     width: 150px;
@@ -56,6 +75,10 @@ export default {
     border-radius: 5px;
     font-family: 'Noto Sans SC', sans-serif;
 }
+
+/* h3{
+    text-decoration: underline;
+} */
 
 .report-button{
     background-color: #474787;
@@ -110,6 +133,44 @@ export default {
     color:white;
 }
 
+
+form{
+  width: 400px;
+  input{ 
+    display: block;
+    width: 100%;
+    margin-bottom: 0.5rem;
+    padding: 1rem;
+    font-size: 15px;
+    border-radius: 5px;
+    border: 1px #474787 solid;
+
+  }
+  textarea{
+    display: block;
+    width: 100%;
+    margin-bottom: 0.5rem;
+    padding: 1rem;
+    font-size: 15px;
+    border-radius: 5px;
+    border: 1px #474787 solid;
+  }
+  .edit-resolution-button{
+    background-color:  #474787;
+    color: white;
+    font-weight: bold;
+    border-radius: 5px;
+    font-size: 15px;
+  }
+  .edit-resolution-button:hover{
+    background-color: white;
+    color:#474787;
+    border: 3px #474787 solid;
+    padding: 0.9rem;
+    cursor: pointer;
+  
+  }
+}
 
 
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,61 @@
 <template>
   <div class="home">
-    <h1>Welcome</h1>
+      <form @submit="addUser" class="new-user">
+        <input class="form-input" type="text" placeholder="Email Address" name="email" />
+        <input class="form-input" type="text" placeholder="Password" name="password"/>
+        <input type="submit" class="add-user-button" value="Sign Up"/>
+          <!-- <img class="main-image" alt="resolute" src="../assets/resolute.png"> -->
+      </form>
   </div>
+
 </template>
 
 <script>
 
 </script>
+
+<style scoped lang="scss">
+*{
+  box-sizing: border-box;
+}
+
+form{
+  background-image: url("../assets/resolute.png");
+   background-repeat: no-repeat;
+   background-position: center;
+   background-size: cover;
+   height: 700px;
+   position: center;
+
+  input{ 
+    margin: 5px;
+    margin-top: 80px;
+    padding: 1rem;
+    font-size: 15px;
+    border-radius: 5px;
+    border: 1px #474787 solid;
+  }
+}
+  .add-user-button{
+    background-color:  #474787;
+    color: white;
+    font-weight: bold;
+    border-radius: 5px;
+    font-size: 15px;
+  }
+  .add-user-button:hover{
+    background-color: white;
+    color:#474787;
+    border: 3px #474787 solid;
+    padding: 0.9rem;
+    cursor: pointer;
+  }
+  
+  .main-image{
+    width: 100%;
+    position: center;
+
+  }
+
+  
+</style>
